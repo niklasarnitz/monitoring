c5ca65ab34a35abe4a47887bdf5aebe55fa9f722
Niklas Arnitz <niklas.arnitz@icloud.com>
2019-04-30 12:30:00 +0200

    First commit, added the network-device-counter, crude but it works
    Changes to be committed:
    	new file:   network-device-counter.py
    	new file:   requirements.txt

---
+++ network-device-counter.py
@@ -0,0 +1,38 @@
+import subprocess
+from influxdb import InfluxDBClient
+from datetime import datetime
+import re
+from time import sleep
+import pprint
+
+dbhost = "localhost"
+dbport = "8086"
+arpRegex = "(?:hosts\/sec).(.*)"
+pp = pprint.PrettyPrinter()
+
+influxClient = InfluxDBClient(host=dbhost, port=dbport)
+
+influxClient.switch_database('network-devices')
+
+def getNetworkDeviceCount():
+    output = subprocess.Popen(["arp-scan", "-l", "-B", "1024000", "-q"], stdout= subprocess.PIPE)
+    stdout,stderr = output.communicate()
+    arpScanOutput = stdout
+    arpScanOutput = "".join(re.findall(arpRegex, str(arpScanOutput)))
+    networkDeviceCount = int(re.findall('-?\d+\.?\d*', arpScanOutput)[0])
+    return networkDeviceCount
+
+while True:
+    currentTime = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+    json_body = [
+        {
+            "measurement": "network-device-count",
+            "time": currentTime,
+            "fields": {
+                "network-device-count": getNetworkDeviceCount()
+            }
+        }
+    ]
+    pp.pprint(json_body)
+    #client.write_points(json_body)
+    sleep(5)
